3a4a1ea77cb8c9cbfe6e63883088d31de894e505
dakesan <dakesan@users.noreply.github.com>
2024-07-19 08:45:31 +0900

    quit quarto

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -192,18 +192,19 @@ return {{
 --     },
 --   },
 -- },
-{ -- requires plugins in lua/plugins/treesitter.lua and lua/plugins/lsp.lua
-    -- for complete functionality (language features)
-    'quarto-dev/quarto-nvim',
-    ft = { 'quarto' },
-    dev = false,
-    opts = {},
-    dependencies = {
-    -- for language features in code cells
-    -- configured in lua/plugins/lsp.lua and
-    -- added as a nvim-cmp source in lua/plugins/completion.lua
-    'jmbuhr/otter.nvim',
-}, {
+-- { -- requires plugins in lua/plugins/treesitter.lua and lua/plugins/lsp.lua
+--     -- for complete functionality (language features)
+--     'quarto-dev/quarto-nvim',
+--     ft = { 'quarto' },
+--     dev = false,
+--     opts = {},
+--     dependencies = {
+--     -- for language features in code cells
+--     -- configured in lua/plugins/lsp.lua and
+--     -- added as a nvim-cmp source in lua/plugins/completion.lua
+--     'jmbuhr/otter.nvim',
+-- }, 
+{
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v2.x',
     config = function()
